5ad33c6b6120fa4bb88e460e7dcf95d39e65c75a
An Nuyen <annuyen@gmail.com>
2018-02-28 23:06:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
@@ -8,15 +8,15 @@ class StudentsController < ApplicationController
   def show
   end
 
-  def activate 
-    set_student 
-    if @student.active 
-      @student.active = false 
-    else @student.active = true 
-    end 
-    @student.save 
+  def activate
+    set_student
+    if @student.active
+      @student.active = false
+    else @student.active = true
+    end
+    @student.save
     redirect_to student_path(@student)
-  end 
+  end
 
   private
 
